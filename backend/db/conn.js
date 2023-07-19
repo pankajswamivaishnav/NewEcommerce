99f29f9b5343fda3fa671bbcca9dc171580bc448
pankaj7073 <pankajvaishnav128@gmail.com>
2023-07-19 19:34:08 +0530

    Changes db

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connection Successfull");
   })
