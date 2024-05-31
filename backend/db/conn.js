405462329c3a6dc913f8efd64626e9aa9ae720a3
pankaj7073 <pankajvaishnav128@gmail.com>
2024-05-31 10:53:48 +0530

    changes

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://0.0.0.0:27017/eCommerceData")
+  // .connect("mongodb://0.0.0.0:27017/eCommerceData")
+  .connect(process.env.MONGO)
   .then(() => {
     console.log(`Connection established`);
   })
